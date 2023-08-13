99acd7bb73d49cf21d69ae0e5a2e52fdc5993251
Gowrish_22 <gowrishankar29102002@gmail.com>
2023-08-13 21:00:36 +0530

    feat: Dark Mode - Completed
    Implemented Dark & Light theme in Tab Bar

--- screens/TabScreen.jsx
+++ screens/TabScreen.jsx
@@ -1,21 +1,25 @@
 
 import React from 'react'
 import { Text, View } from 'react-native'
-import { styles } from '../styles/Login'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import ProductScreen from './ProductScreen';
 import ProfileScreen from './ProfileScreen';
 import DrawerScreen from './DrawerScreen';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Entypo } from '@expo/vector-icons'; 
+import { Entypo } from '@expo/vector-icons';
 import Drawers, { FeedbackScreen, NotificationsScreen } from './DrawerScreen';
 import PostListScreen from './PostListScreen';
+import { useColorSchemeContext } from '../theme/ColorSchemeContext';
+import { DarkTheme } from '@react-navigation/native';
 
 
 const Tab = createBottomTabNavigator();
 
 
 const TabScreen = () => {
+
+    const { colorScheme } = useColorSchemeContext();
+
     return (
         <Tab.Navigator
             screenOptions={({ route }) => ({
@@ -23,14 +27,15 @@ const TabScreen = () => {
                 headerStyle: { backgroundColor: '#00A8E8' },
                 headerTintColor: '#fff',
                 headerTitleStyle: { fontWeight: 'bold' },
-                tabBarActiveTintColor: '#00A8E8',
-                tabBarInactiveTintColor: 'gray',
+                tabBarStyle: { backgroundColor: colorScheme === 'dark' ? '#000' : '#00A8E8', borderBlockColor: colorScheme === 'dark' ? '#000' : '#00A8E8', },
+                tabBarActiveTintColor: colorScheme === 'dark' ? '#fff' : '#000',
+                tabBarInactiveTintColor: colorScheme === 'dark' ? 'gray' :  '#fff',
                 tabBarIcon: ({ focused, color, size }) => {
                     let iconName;
-                    if (route.name === 'Home') {
+                    if (route.name === 'PostList') {
                         iconName = focused
-                            ? 'home'
-                            : 'home-outline';
+                            ? 'post'
+                            : 'post-outline';
                     } else if (route.name === 'Products') {
                         iconName = focused
                             ? 'shopping'
@@ -51,7 +56,7 @@ const TabScreen = () => {
             })}
             initialRouteName='HomeTab'
         >
-            <Tab.Screen name='Home' component={Drawers } />
+            <Tab.Screen name='PostList' component={PostListScreen} />
             <Tab.Screen name='Products' component={ProductScreen} />
             <Tab.Screen name='Profile' component={ProfileScreen} />
         </Tab.Navigator>
