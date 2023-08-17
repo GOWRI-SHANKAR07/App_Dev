2aa6b405dcf8e0666da40de53ce355382cf5c674
Gowrish_22 <gowrishankar29102002@gmail.com>
2023-08-17 18:31:52 +0530

    Func: Toggle Logout button added: Completed

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { SafeAreaView, Text, StyleSheet } from 'react-native';
 import { useColorSchemeContext } from '../theme/ColorSchemeContext';
 import { Logout, ToggleTheme } from '../components/ToggleButton';
+import { TouchableOpacity } from 'react-native';
+import { styles } from '../styles/Profile';
+import { useAuthContext } from '../Auth/AuthContext';
 
-const ProfileScreen = () => {
+const ProfileScreen = ({navigation}) => {
   const { colorScheme } = useColorSchemeContext(); // Access the color scheme
+  const { logout } = useAuthContext();
 
   const containerStyle = {
     ...styles.container,
@@ -16,27 +20,25 @@ const ProfileScreen = () => {
     color: colorScheme === 'dark' ? '#fff' : '#000',
   };
 
+  const handleLogout = () => {
+    logout();
+    navigation.navigate('Login');
+  }
+
   return (
     <SafeAreaView style={containerStyle}>
       <Text style={textStyle}>Welcome to Home Screen</Text>
       <Text style={textStyle}>{colorScheme === 'dark' ? 'Dark Mode' : 'Light Mode'}</Text>
       <ToggleTheme />
-      <Logout />
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: colorScheme === 'dark' ? '#fff' : '#000' }]}
+        onPress={handleLogout}
+      >
+        <Text style={[styles.buttonText, { color: colorScheme === 'dark' ? '#000' : '#fff' }]}>Logout</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    paddingBottom: 10,
-  },
-});
 
 export default ProfileScreen;
