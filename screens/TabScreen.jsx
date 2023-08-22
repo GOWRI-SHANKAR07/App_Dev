1a8fb14fe4b511996171387404a0043efb12865d
Gowrish_22 <gowrishankar29102002@gmail.com>
2023-08-22 12:34:20 +0530

    Issue: Tab Screen: updated the route conditional render using switch case : Completed

--- screens/TabScreen.jsx
+++ screens/TabScreen.jsx
@@ -7,7 +7,7 @@ import ProfileScreen from './ProfileScreen';
 import DrawerScreen from './DrawerScreen';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Entypo } from '@expo/vector-icons';
-import Drawers, { FeedbackScreen} from './DrawerScreen';
+import Drawers, { FeedbackScreen } from './DrawerScreen';
 import { NotificationsScreen } from './NotificationScreen';
 import PostListScreen from './PostListScreen';
 import { useColorSchemeContext } from '../theme/ColorSchemeContext';
@@ -33,22 +33,29 @@ const TabScreen = () => {
                 headerTitleStyle: { fontWeight: 'bold' },
                 tabBarStyle: { backgroundColor: colorScheme === 'dark' ? '#000' : '#00A8E8', borderBlockColor: colorScheme === 'dark' ? '#000' : '#00A8E8', },
                 tabBarActiveTintColor: colorScheme === 'dark' ? '#fff' : '#000',
-                tabBarInactiveTintColor: colorScheme === 'dark' ? 'gray' :  '#fff',
+                tabBarInactiveTintColor: colorScheme === 'dark' ? 'gray' : '#fff',
                 tabBarIcon: ({ focused, color, size }) => {
                     let iconName;
-                    if (route.name === 'Products') {
-                        iconName = focused
-                            ? 'post'
-                            : 'post-outline';
-                    } else if (route.name === 'Notifications') {
-                        iconName = focused
-                            ? 'shopping'
-                            : 'shopping-outline';
-                    } else if (route.name === 'Profile') {
-                        iconName = focused
-                            ? 'account'
-                            : 'account-outline';
+                    switch (route.name) {
+                        case 'Products':
+                            iconName = focused
+                                ? 'post'
+                                : 'post-outline';
+                            break;
+
+                        case 'Notifications':
+                            iconName = focused
+                                ? 'shopping'
+                                : 'shopping-outline';
+                            break;
+
+                        case 'Profile':
+                            iconName = focused
+                                ? 'account'
+                                : 'account-outline';
+                            break;
                     }
+
                     return (
                         <MaterialCommunityIcons
                             name={iconName}
